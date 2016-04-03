a0e50ec70f147fb6f39513ef9543de79aed0450c
geptto <geptto@users.noreply.github.com>
2016-04-02 23:33:17 -0600

    Update testdev0-revert.sh

--- testdev0-revert.sh
+++ testdev0-revert.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -e
 
+#Revert the hostname setup
 sudo rm /etc/hostname
 sudo mv /etc/hostname.bak /etc/hostname
+
+#Revert persistent names to the wireless cards
+sudo rm /etc/udev/rules.d/10-wifi.rules
