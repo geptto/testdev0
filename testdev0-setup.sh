4dfc0d238f1be3e3cde8087437f881567e1185ce
geptto <geptto@users.noreply.github.com>
2016-04-02 23:30:30 -0600

    Update testdev0-setup.sh

--- testdev0-setup.sh
+++ testdev0-setup.sh
@@ -1,7 +1,15 @@
 #!/bin/bash -e
 
+#Set the device hostname
 sudo mv /etc/hostname /etc/hostname.bak
 cat <<EOF | sudo tee /etc/hostname > /dev/null
 testdev0
 EOF
 sudo chmod 644 /etc/hostname
+
+#Set persistent names to the wireless cards
+cat <<EOF | sudo tee /etc/udev/rules.d/10-wifi.rules > /dev/null
+SUBSYSTEM=="net", ATTRS{idVendor}=="148f", ATTRS{idProduct}=="5370", NAME="wifi0"
+SUBSYSTEM=="net", ATTRS{idVendor}=="148f", ATTRS{idProduct}=="7601", NAME="wifi1"
+EOF
+sudo chmod 644 /etc/udev/rules.d/10-wifi.rules
