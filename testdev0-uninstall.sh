cca0355cb4a1965bef09482bcced7e424154a4e7
geptto <geptto@users.noreply.github.com>
2016-04-03 17:33:01 -0600

    Update testdev0-uninstall.sh

--- testdev0-uninstall.sh
+++ testdev0-uninstall.sh
@@ -1,8 +1,17 @@
 #!/bin/bash -e
 
 #Revert the hostname setup
-sudo rm /etc/hostname
 sudo mv /etc/hostname.bak /etc/hostname
 
 #Revert persistent names to the wireless cards
 sudo rm /etc/udev/rules.d/10-wifi.rules
+sudo mv /etc/wpa_supplicant/wpa_supplicant.conf.old /etc/wpa_supplicant/wpa_supplicant.conf
+sudo mv /etc/network/interfaces.old /etc/network/interfaces
+sudo mv /etc/dhcp/dhcpd.conf.old /etc/dhcp/dhcpd.conf
+sudo mv /etc/default/isc-dhcp-server.old /etc/default/isc-dhcp-server
+sudo mv /etc/hostapd/hostapd.conf.old /etc/hostapd/hostapd.conf
+sudo mv /etc/default/hostapd.old /etc/default/hostapd
+sudo mv /etc/sysctl.conf.old /etc/sysctl.conf
+sudo mv /etc/iptables.ipv4.nat.old /etc/iptables.ipv4.nat
+
+sudo apt-get purge isc-dhcp-server hostapd || true
